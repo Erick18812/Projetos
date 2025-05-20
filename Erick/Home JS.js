5fe167b2e4f2a87c046c7811ebda067e58b40071
erick <erickoborges13@gmail.com>
2025-05-20 13:47:43 -0300

    20/05/2025

--- Erick/Home JS.js
+++ Erick/Home JS.js
@@ -1,21 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>botão</title>
-    <link rel="stylesheet" href="Página de Login1.html">
-</head>
-    <body>
+function teste(){
 
-    <button onclick="teste()" id="login">Alterar Tema</button>
-    <script>
-        function teste() {
+    let user = document.getElementById('user').value;
+    let senha = document.getElementById('senha').value;
 
-            let user = document.getElementById('user').value;
-            let senha = document.getElementById('senha').value;
-        }
-    </script>
-</body>
-</body>
-</html>
+    if(user==='Admin' && senha==='1234')
+      window.location.href="Página de Login2.html";
+    else
+      alert('Tentar novamente')
+    
+    }
+
+    function mudarcor () {
+    document.querySelector("body").style.backgroundColor= "black";
+    document.querySelector("h1").style.color= "black" 
+    document.querySelector("p").style.color= "white";
+  }
+
+  function mudar () {
+    document.querySelector("body").style.backgroundColor= "black";
+    document.querySelector("h1").style.color= "white";
+    document.querySelector("p").style.color= "white";
+  }
+
+    function Voltar () {
+    window.location.href="Página de login 1.html"
+   }
